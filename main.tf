e36e2b36c7311bd33000725f48fdf33495199bc6
Noresh <noreshdhoni@gmail.com>
2024-08-25 09:10:49 +0530

    s3 backend

--- main.tf
+++ main.tf
@@ -159,3 +159,14 @@ resource "aws_instance" "jenkins" {
 output "jenkins_public_ip" {
   value = aws_instance.jenkins.public_ip
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "statefilebucketnoresh"
+    key            = "terraform/state"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock"
+  }
+}
+
+
